48c5cb5e97a004467fb53161ab862687b63f24f7
Victoria Wang <victoriawang29@gmail.com>
2022-04-06 13:58:12 -0500

    Deleted owl

--- WordDoctor/settings.py
+++ WordDoctor/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x3e(db3)$3cvd88rl$b_)f1hflm5m(ms$vyzse*ql_=4$%+ql6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
